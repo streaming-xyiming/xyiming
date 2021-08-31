bb03de5611804baa0006314998e665c75bac5bf7
Alex Kouprin <kpr@nearprotocol.com>
2021-08-31 13:24:27 +0100

    Updates

---
+++ precache-manifest.8ac8efdb30e7087f54866ec58d8d120f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2396773ecf43263634dcc5bd01da6290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b15ac3f80273e6422db",
+    "url": "/static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "0d8540ac8b99d044e7e2",
+    "url": "/static/css/main.b793ea44.chunk.css"
+  },
+  {
+    "revision": "5b15ac3f80273e6422db",
+    "url": "/static/js/2.e706c718.chunk.js"
+  },
+  {
+    "revision": "719e0f002bbd20db754fff0ad1d1a154",
+    "url": "/static/js/2.e706c718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8540ac8b99d044e7e2",
+    "url": "/static/js/main.8f8028d9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e9cb853c020ccb953419551605db7251",
+    "url": "/static/media/logo.e9cb853c.png"
+  }
+]);
